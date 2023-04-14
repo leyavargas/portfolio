8afc7ba1a2f9c591809b7ce06051d4a8e3153ee4
leyavargas <127598945+leyavargas@users.noreply.github.com>
2023-04-14 23:17:05 +0200

    prueba 2

--- wp-includes/js/dist/url.js
+++ wp-includes/js/dist/url.js
@@ -638,7 +638,7 @@ function isValidProtocol(protocol) {
  * @example
  * ```js
  * const authority1 = getAuthority( 'https://wordpress.org/help/' ); // 'wordpress.org'
- * const authority2 = getAuthority( 'https://example.com:8080/test/' ); // 'localhost:8080'
+ * const authority2 = getAuthority( 'https://leyavargas.github.io/portfolio:8080/test/' ); // 'localhost:8080'
  * ```
  *
  * @return {string|void} The authority part of the URL.
@@ -681,7 +681,7 @@ function isValidAuthority(authority) {
  *
  * @example
  * ```js
- * const path1 = getPath( 'https://example.com:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
+ * const path1 = getPath( 'https://leyavargas.github.io/portfolio:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
  * const path2 = getPath( 'https://wordpress.org/help/faq/' ); // 'help/faq'
  * ```
  *
@@ -725,7 +725,7 @@ function isValidPath(path) {
  *
  * @example
  * ```js
- * const queryString = getQueryString( 'https://example.com:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
+ * const queryString = getQueryString( 'https://leyavargas.github.io/portfolio:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
  * ```
  *
  * @return {string|void} The query string part of the URL.
@@ -836,7 +836,7 @@ function isValidQueryString(queryString) {
  *
  * @example
  * ```js
- * const pathAndQueryString1 = getPathAndQueryString( 'https://example.com:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
+ * const pathAndQueryString1 = getPathAndQueryString( 'https://leyavargas.github.io/portfolio:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
  * const pathAndQueryString2 = getPathAndQueryString( 'https://wordpress.org/help/faq/' ); // '/help/faq'
  * ```
  *
@@ -860,7 +860,7 @@ function getPathAndQueryString(url) {
  *
  * @example
  * ```js
- * const fragment1 = getFragment( 'https://example.com:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
+ * const fragment1 = getFragment( 'https://leyavargas.github.io/portfolio:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
  * const fragment2 = getFragment( 'https://wordpress.org#another-fragment?query=true' ); // '#another-fragment'
  * ```
  *
@@ -1288,7 +1288,7 @@ function cleanForSlug(string) {
  *
  * @example
  * ```js
- * const filename1 = getFilename( 'https://example.com:8080/this/is/a/test.jpg' ); // 'test.jpg'
+ * const filename1 = getFilename( 'https://leyavargas.github.io/portfolio:8080/this/is/a/test.jpg' ); // 'test.jpg'
  * const filename2 = getFilename( '/this/is/a/test.png' ); // 'test.png'
  * ```
  *
